bcc4ae869e257ae424b7b21816a1d1d1c65a77d1
Brown Scholar <brownscholar@BridgeUP-LAP-321.local>
2021-03-18 18:23:21 -0400

    new changes

--- 3-16/intern-test.sh
+++ 3-16/intern-test.sh
@@ -1,3 +1,28 @@
-#!/bin/bash
- 
- ls 
+# !/bin/bash
+
+STRING="START"
+echo $STRING
+
+cd /Users/brownscholar/documents/intern/ocean-motion-2021/2-25
+
+#compiling files
+gfortran -O3 -o vectorq.exe vectorq.f
+gfortran -O3 -o omegainv.exe omegainv.f
+
+dateList= '/Users/brownscholar/documents/intern/ocean-motion-2021/3-16/date_list.txt'
+
+while read p; do
+	echo $p
+
+	#creating exec files with all dates
+	python writing-exec-files-1.py $p
+	python writing-exec-files-2.py $p
+
+	#running exec files
+	./vectorq.exec
+	./omegainv.exec
+	
+done </Users/brownscholar/documents/intern/ocean-motion-2021/3-16/date_list.txt
+
+STRING="DONE"
+echo $STRING
